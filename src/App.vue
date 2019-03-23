29f021671290c3677d4ed6fa085088f590df08ed
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2019-03-23 10:12:45 +0100

    base setup

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <!-- Shows if there are no students -->
+    <template v-if="!studentAvaliable">
+      <h1 class="text-danger">Students are not avaliable</h1>
+    </template>
+    <!-- Shows if there are students added -->
+    <template v-else>
+      <h1>There is students</h1>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+
+<script>
+export default {
+  data() {
+    return {
+      studentAvaliable: false
+    };
+  },
+  methods: {},
+  created() {}
+};
+</script>
